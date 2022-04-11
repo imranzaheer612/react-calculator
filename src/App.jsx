fe46f75227b9ee4f43f77f6af1ca5bec81169004
imran <izaheer612@gmail.com>
2022-04-11 23:52:26 +0500

    changed to functional components and used hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Keys from './components/keys';
 import Screen from './components/screen';
 
 
+function App() {
 
-class App extends Component {
-    state = { 
-        expression : "", 
-    } 
+    const [expr, setExpr] = useState('')
 
     /**
      * handling key event
      * @param value: value of the key pressed
     */
-    handelKey = (value) => {
-        var expression = this.state.expression + String(value)
-        this.setState({
-            expression : expression
-        })
+    const handelKey = (value) => {
+        setExpr(expr + String(value));
     }
+   
+    return (
+        <div className='calculator'>
+            <Screen expression={ expr } />
+            <Keys 
+            expression={ expr }
+            onClick={ handelKey }
+            onEval={() => setExpr(eval(expr))} 
+            onClear={() => setExpr(expr.slice(0, -1))} 
+            onClearAll={ () => setExpr("")}
+            />
+        </div>
+    );
 
-    /**
-     * handle evaluation event to get the answer
-     * of the expression
-    */
-    handleEqual = () => {
-        this.setState({
-            expression: eval(this.state.expression)
-        })
-    }
-
-    /**
-     * to clear the screen
-    */
-    handleClearAll = () => {
-        this.setState({
-            expression : '', 
-        })
-    }
-
-    /**
-     * backspace
-    */
-    handleClear = () => {
-        this.setState({
-            expression: (this.state.expression).slice(0, -1)
-        })
-    }
-
-
-    render() { 
-        return (
-            <div className='calculator'>
-                <Screen expression={ this.state.expression } />
-                <Keys expression={ this.state.expression } onClick={this.handelKey} onEval={this.handleEqual} onClear={this.handleClear} onClearAll={this.handleClearAll}/>
-            </div>
-        );
-    }
 }
- 
+
 export default App;
--- src/components/keys.jsx
+++ src/components/keys.jsx
@@ -1,42 +1,40 @@
 import React, { Component } from 'react';
 
-class Keys extends Component {
+function Keys (props) {
 
-    render() { 
-
-        /**
+    /**
          * defining css classes for each button 
         */
-        let btn = "btn btn-light waves-effect cal-btn";
-        let equal =  btn +' equal';
-        let plus = btn + ' plus';
-        let minus = btn + ' minus';
-        let divide = btn + ' divide';
-        let cross = btn + ' cross';
+     let btn = "btn btn-light waves-effect cal-btn";
+     let equal =  btn +' equal';
+     let plus = btn + ' plus';
+     let minus = btn + ' minus';
+     let divide = btn + ' divide';
+     let cross = btn + ' cross';
 
-        return ( 
-            <div className='cal-keys'>
-                <button className={btn} onClick={(e) => this.props.onClick(7, e)} >7</button>
-                <button className={btn} onClick={(e) => this.props.onClick(8, e)} >8</button>
-                <button className={btn} onClick={(e) => this.props.onClick(9, e)} >9</button>
-                <button className={btn} onClick={(e) => this.props.onClick(4, e)} >4</button>
-                <button className={btn} onClick={(e) => this.props.onClick(5, e)} >5</button>
-                <button className={btn} onClick={(e) => this.props.onClick(6, e)} >6</button>
-                <button className={btn} onClick={(e) => this.props.onClick(1, e)} >1</button>
-                <button className={btn} onClick={(e) => this.props.onClick(2, e)} >2</button>
-                <button className={btn} onClick={(e) => this.props.onClick(3, e)} >3</button>
-                <button className={btn + ' zero'}  onClick={(e) => this.props.onClick(0, e)} >0</button>
-                <button className={btn} onClick={(e) => this.props.onClick('.', e)} >.</button>
-                <button className={btn + ' clear-all'} onClick={this.props.onClearAll}>C</button>
-                <button className={btn + ' clear'} onClick={this.props.onClear}>{"<-"}</button>
-                <button className={plus} onClick={(e) => this.props.onClick('+', e)} >+</button>
-                <button className={minus} onClick={(e) => this.props.onClick('-', e)} >-</button>
-                <button className={cross} onClick={(e) => this.props.onClick('*', e)} >X</button>
-                <button className={divide} onClick={(e) => this.props.onClick('/', e)} >/</button>
-                <button className={equal} onClick={() => this.props.onEval()}>=</button>
-            </div>
-        );
-    }
+    return (
+        <div className='cal-keys'>
+            <button className={btn} onClick={(e) => props.onClick(7, e)} >7</button>
+            <button className={btn} onClick={(e) => props.onClick(8, e)} >8</button>
+            <button className={btn} onClick={(e) => props.onClick(9, e)} >9</button>
+            <button className={btn} onClick={(e) => props.onClick(4, e)} >4</button>
+            <button className={btn} onClick={(e) => props.onClick(5, e)} >5</button>
+            <button className={btn} onClick={(e) => props.onClick(6, e)} >6</button>
+            <button className={btn} onClick={(e) => props.onClick(1, e)} >1</button>
+            <button className={btn} onClick={(e) => props.onClick(2, e)} >2</button>
+            <button className={btn} onClick={(e) => props.onClick(3, e)} >3</button>
+            <button className={btn + ' zero'}  onClick={(e) => props.onClick(0, e)} >0</button>
+            <button className={btn} onClick={(e) => props.onClick('.', e)} >.</button>
+            <button className={btn + ' clear-all'} onClick={props.onClearAll}>C</button>
+            <button className={btn + ' clear'} onClick={props.onClear}>{"<-"}</button>
+            <button className={plus} onClick={(e) => props.onClick('+', e)} >+</button>
+            <button className={minus} onClick={(e) => props.onClick('-', e)} >-</button>
+            <button className={cross} onClick={(e) => props.onClick('*', e)} >X</button>
+            <button className={divide} onClick={(e) => props.onClick('/', e)} >/</button>
+            <button className={equal} onClick={() => props.onEval()}>=</button>
+        </div>
+    
+    );
 }
  
 export default Keys;
